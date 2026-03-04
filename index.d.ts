89a2ecdce39f7fd56148f2b879f789210884e689
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-03-04 14:06:31 -0500

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 export default plugin;
 declare namespace plugin {
-    let configs: {};
-    namespace meta {
-        let name: any;
-        let namespace: string;
-        let version: any;
-    }
-    let rules: {
-        "separate-type-imports": import("eslint").Rule.RuleModule;
-    };
+  let configs: {};
+  namespace meta {
+    let name: any;
+    let namespace: string;
+    let version: any;
+  }
+  let rules: {
+    "separate-type-imports": import("eslint").Rule.RuleModule;
+  };
 }
