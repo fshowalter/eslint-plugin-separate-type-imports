4b8fbfafe21d9067a882d290147287a9a7dcaee8
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-03-04 14:09:58 -0500

    try/fix types (#20)
    * try/fix types
    
    * fix
    
    * knip
    
    * format

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,12 @@
-import type { Linter } from "eslint";
-
-declare const plugin: {
-  readonly meta: {
-    readonly name: string;
-    readonly version: string;
-  };
-  readonly configs: {
-    readonly recommended: { readonly rules: Readonly<Linter.RulesRecord> };
-    readonly all: { readonly rules: Readonly<Linter.RulesRecord> };
-  };
-};
-
 export default plugin;
+declare namespace plugin {
+  let configs: {};
+  namespace meta {
+    let name: any;
+    let namespace: string;
+    let version: any;
+  }
+  let rules: {
+    "separate-type-imports": import("eslint").Rule.RuleModule;
+  };
+}
